13c5c41ac5e29a94944c018b9500b977ffa5b3f1
dc <dingchao2002415@126.com>
2023-09-05 14:06:08 +0800

    注释增加

--- base/BasePage.py
+++ base/BasePage.py
@@ -1,6 +1,6 @@
 """
 @FileName：BasePage.py\n
-@Description：基础selenium页面\n
+@Description：基础selenium页面(basepage)\n
 @Author：Mr.Dc\n
 @Time：2023/9/5 11:28\n
 @Department：研发部\n
@@ -21,14 +21,29 @@ class BasePage:
 
     # 定位元素
     def locate_element(self, args):
+        """
+        寻找element
+        :param args:
+        :return:driver
+        """
         return self.driver.find_element(*args)
 
     # 定位一组元素
     def locate_eles(self, args):
+        """
+        根据通配符获取所有的element
+        :param args:
+        :return:elements(所有匹配的element)
+        """
         return self.driver.find_elements(*args)
 
     # 从一组元素中获取值
     def gettext_from_eles(self, args):
+        """
+        根据通配符获取所有的值
+        :param args:
+        :return:elements(所有匹配的element的data值)
+        """
         elements = self.locate_eles(args)
         data = []
         try:
@@ -41,6 +56,12 @@ class BasePage:
 
     #
     def getele_from_eles(self, args, ele_name: str):
+        """
+        根据text从一类elements中获取匹配的ele
+        :param args:
+        :param ele_name:匹配的字符
+        :return:ele(值匹配的element)
+        """
         elements = self.locate_eles(args)
         try:
             for ele in elements:
@@ -52,6 +73,12 @@ class BasePage:
 
     # 输入值
     def input_(self, args, text):
+        """
+         给element输入值
+         :param text: 准备赋的字符串
+         :param args:
+         :return:None
+         """
         self.locate_element(args).send_keys(text)
 
     # 点击按钮
